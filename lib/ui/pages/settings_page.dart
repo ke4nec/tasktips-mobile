84eafdd2939537d8b00f15b5e72c3e2fbbd9e1a2
ke4nec <ke4nec@qq.com>
2026-09-17 19:42:25 +0800

    chore(release): 版本 0.0.2

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -17,7 +17,7 @@ class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key, required this.model});
 
   // 与 pubspec.yaml versionName 保持一致
-  static const appVersion = '0.0.1';
+  static const appVersion = '0.0.2';
 
   @override
   Widget build(BuildContext context) {
